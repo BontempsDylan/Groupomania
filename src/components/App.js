6eb53285abf097d3b70697c21d0923a1ce841529
didisxm97166 <dylanbontemps@hotmail.fr>
2022-09-23 14:39:33 +0200

    installation de sass et premiere ligne frontend

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
-import logo from '../assets/icon-left-font.svg';
-import '../styles/App.css';
+import logo from '../assets/icon-left-font-removebg-preview.png';
+import '../styles/CSS/main.css';
 
 function App() {
   return (
     
       <header className="header">
-        <div className='header__logo'><img src={logo} className="header__logo__img" alt="logo" /></div> 
-        <div className='header__'>
-          <div className="header__connection"><p>connection</p></div>
-          <div className="header__inscription"><p>inscrivez-vous</p></div>
+        <div className='header__logo'><img src={logo} alt="logo" /></div> 
+        <div className='header__bloc'>
+          <div className="header__bloc__connection"><p>connection</p></div>
+          <div className="header__bloc__inscription"><p>inscrivez-vous</p></div>
         </div>
       </header>
     
